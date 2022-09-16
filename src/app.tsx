d92ddeb852ba7d7990e0f6dd773fb970658b6102
Damien Sedgwick <damiensedgwick@icloud.com>
2022-09-16 10:39:38 +0100

    useReducer functionality working as expected.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,59 @@
+import { useReducer } from "react";
 import styled from "styled-components";
+
 // @ts-ignore
 import avatar from "./bean.jpeg";
+
+type Data = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
+
+type Action =
+  { type: "UPDATE_FIRST_NAME", value: string }
+  | { type: "UPDATE_LAST_NAME", value: string }
+  | { type: "UPDATE_EMAIL", value: string }
+  | { type: "UPDATE_PHONE", value: string }
+
+const initialState: Data = { firstName: "", lastName: "", email: "", phone: "" };
+
+const handleFormInputChange = (state: Data, action: Action) => {
+  switch (action.type) {
+    case "UPDATE_FIRST_NAME":
+      return { ...state, firstName: action.value };
+    case "UPDATE_LAST_NAME":
+      return { ...state, lastName: action.value };
+    case "UPDATE_EMAIL":
+      return { ...state, email: action.value };
+    case "UPDATE_PHONE":
+      return { ...state, phone: action.value };
+    default:
+      throw new Error("Unhandled form input action");
+  }
+};
+
+export const App = () => {
+  const [state, dispatch] = useReducer(handleFormInputChange, initialState);
+
+  return (
+    <Container>
+      <ProfileCard>
+        <UserDetailsForm>
+          <Avatar src={avatar} alt="the users avatar" />
+          <FormGroup>
+            <Input placeholder="First Name" onChange={(e) => dispatch({ type: "UPDATE_FIRST_NAME", value: e.target.value })} />
+            <Input placeholder="Last Name" onChange={(e) => dispatch({ type: "UPDATE_LAST_NAME", value: e.target.value })} />
+            <Input placeholder="Email" onChange={(e) => dispatch({ type: "UPDATE_EMAIL", value: e.target.value })} />
+            <Input placeholder="Phone" onChange={(e) => dispatch({ type: "UPDATE_PHONE", value: e.target.value })} />
+          </FormGroup>
+          <Button type="button" onClick={() => console.log(state)}>Save</Button>
+        </UserDetailsForm>
+      </ProfileCard>
+    </Container>
+  );
+};
 
 const Container = styled.div`
   width: 100%;
@@ -73,29 +126,10 @@ const Button = styled.button`
   border-radius: 8px;
   box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
   transition: 250ms ease-in-out;
-  
+
   &:hover, &:active, &:focus {
     transition: 250ms ease-in-out;
     transform: scale(1.1);
     outline: none;
   }
 `;
-
-export const App = () => {
-  return (
-    <Container>
-      <ProfileCard>
-        <UserDetailsForm>
-          <Avatar src={avatar} alt="the users avatar" />
-          <FormGroup>
-            <Input placeholder="First Name" />
-            <Input placeholder="Last Name" />
-            <Input placeholder="Email" />
-            <Input placeholder="Phone" />
-          </FormGroup>
-          <Button type="button">Save</Button>
-        </UserDetailsForm>
-      </ProfileCard>
-    </Container>
-  );
-};
